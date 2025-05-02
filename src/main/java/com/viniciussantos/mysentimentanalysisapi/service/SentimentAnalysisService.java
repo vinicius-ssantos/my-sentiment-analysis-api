0c48017c3ddf62485aae1f3296ea5c08200f65d1
Vinicius <viniciusolicveira@gmail.com>
2025-05-02 19:13:42 -0300

    refactor: adapta serviço de análise para fluxo reativo com WebFlux

--- src/main/java/com/viniciussantos/mysentimentanalysisapi/service/SentimentAnalysisService.java
+++ src/main/java/com/viniciussantos/mysentimentanalysisapi/service/SentimentAnalysisService.java
@@ -5,6 +5,7 @@ import com.viniciussantos.mysentimentanalysisapi.client.HuggingFaceClient;
 import com.viniciussantos.mysentimentanalysisapi.model.SentimentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class SentimentAnalysisService {
@@ -12,8 +13,8 @@ public class SentimentAnalysisService {
     @Autowired
     private HuggingFaceClient huggingFaceClient;
 
-    public SentimentResponse analyzeText(String comment) {
-        String sentiment = huggingFaceClient.analyzeSentiment(comment).block();
-        return new SentimentResponse(sentiment, 1.0);
+    public Mono<SentimentResponse> analyzeText(String comment) {
+        return huggingFaceClient.analyzeSentiment(comment)
+                .map(result -> new SentimentResponse(result.sentiment(), result.score(), result.dominantLabel()));
     }
 }

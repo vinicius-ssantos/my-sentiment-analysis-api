64e0f540f7c1e05bfe6bdcecc8eb6d9a324152d4
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-05-07 22:46:17 -0300

    refactor(model): converte CommentRequest para record e remove setters

--- src/main/java/com/viniciussantos/mysentimentanalysisapi/model/CommentRequest.java
+++ src/main/java/com/viniciussantos/mysentimentanalysisapi/model/CommentRequest.java
@@ -1,13 +1,19 @@
 package com.viniciussantos.mysentimentanalysisapi.model;
 
-public class CommentRequest {
-    private String comment;
-
+/**
+ * Represents a request containing a comment to be analyzed.
+ * Implemented as a record for immutability and conciseness.
+ *
+ * @param comment The text comment to analyze
+ */
+public record CommentRequest(String comment) {
+    /**
+     * Returns the comment text.
+     * This method is provided for compatibility with frameworks expecting JavaBean conventions.
+     *
+     * @return the comment text
+     */
     public String getComment() {
         return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
     }
 }

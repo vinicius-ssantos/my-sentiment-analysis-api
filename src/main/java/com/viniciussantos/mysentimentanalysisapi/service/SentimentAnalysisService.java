b01e1d0cc64165c477bb1a52e45a8c7c3c0685f5
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-05-07 22:46:32 -0300

    refactor(service): injeta HuggingFaceClient via construtor e documenta serviço

--- src/main/java/com/viniciussantos/mysentimentanalysisapi/service/SentimentAnalysisService.java
+++ src/main/java/com/viniciussantos/mysentimentanalysisapi/service/SentimentAnalysisService.java
@@ -3,15 +3,25 @@ package com.viniciussantos.mysentimentanalysisapi.service;
 
 import com.viniciussantos.mysentimentanalysisapi.client.HuggingFaceClient;
 import com.viniciussantos.mysentimentanalysisapi.model.SentimentResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+/**
+ * Service responsible for analyzing text sentiment using the Hugging Face API.
+ */
 @Service
 public class SentimentAnalysisService {
 
-    @Autowired
-    private HuggingFaceClient huggingFaceClient;
+    private final HuggingFaceClient huggingFaceClient;
+
+    /**
+     * Creates a new SentimentAnalysisService with the required dependencies.
+     * 
+     * @param huggingFaceClient the client used to communicate with Hugging Face API
+     */
+    public SentimentAnalysisService(HuggingFaceClient huggingFaceClient) {
+        this.huggingFaceClient = huggingFaceClient;
+    }
 
     public Mono<SentimentResponse> analyzeText(String comment) {
         return huggingFaceClient.analyzeSentiment(comment)
